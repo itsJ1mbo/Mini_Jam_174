1d903473ac16544583e4c33ec331513a4f52c823
David-104 <dcanelad@ucm.es>
2024-12-21 14:06:19 +0100

    Versión básica del Dungeon Master done

--- Assets/DungeonMaster.cs
+++ Assets/DungeonMaster.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Numerics;
+using Unity.VisualScripting;
 using UnityEngine;
-
 [Flags] public enum Flags 
 {
     /// <summary>
@@ -22,20 +22,50 @@ using UnityEngine;
     TeaPrepared = 64,
     DrankTea = 128
 }
+
+public enum TimePeriod
+{
+    Morning = 0,
+    Afternoon = 1,
+    Evening = 2
+}
+
 public class DungeonMaster : MonoBehaviour
 {
+    private const float MINUTE_LENGTH = 60.0F;
+
     #region Variables
     public DungeonMaster Instance { get; private set; }
-    
+    /// <summary>
+    /// Periodo del dia actual
+    /// </summary>
+    private TimePeriod _currentTimePeriod = TimePeriod.Morning;
+    /// <summary>
+    /// Estado actual de las flags del juego
+    /// </summary>
     private Flags _currentFlags = Flags.None;
-    private List<GameObject> _characters = new List<GameObject>();
-    
-    [Tooltip("Duración de cada periodo de tiempo del juego en minutos")]
+    /// <summary>
+    /// Lista con todas las entidades (objetos/personajes) del juego que cambian de estado dependiendo de la etapa del dia
+    /// </summary>
+    private List<GameObject> _entities = new List<GameObject>();
+    /// <summary>
+    /// Duracion de las etapas del juego
+    /// </summary>
+    [Tooltip("Duración de cada periodo de tiempo del juego en minutos")] 
     [SerializeField] private int _timePeriodLength = 5;
+    /// <summary>
+    /// Timer interno para las etapas del juego
+    /// </summary>
     private float _timePeriodTimer = 0.0f;
+    /// <summary>
+    /// Booleano que controla si queremos que el tiempo corra o no
+    /// </summary>
+    private bool _runTimer = false;
     #endregion
     
     #region Functions
+    
+    #region Unity Callbacks
     void Awake()
     {
         if(Instance == null)
@@ -51,25 +81,52 @@ public class DungeonMaster : MonoBehaviour
 
     void Start()
     {
+        _runTimer = true;
         UpdateGameState();
     }
 
     void Update()
     {
-        
-    }
-    
-    public void SetFlag(Flags flag) { _currentFlags |= flag; }
-    public void RemoveFlag (Flags flag) { _currentFlags &= ~flag; }
-    public void AddCharacter (GameObject character) { _characters.Add(character); }
-    public void RemoveCharacter (GameObject character) { _characters.Remove(character); }
-    private void UpdateGameState()
-    {
-        foreach (GameObject character in _characters)
+        if (_runTimer)
         {
-            //Llamar a la cosa que actualice a cada personaje
-            //character.GetComponent<>().updateState(_currentFlags);
+            _timePeriodTimer += Time.deltaTime;
+            //Debug.Log(_timePeriodTimer);
+            if(_timePeriodTimer >= _timePeriodLength * MINUTE_LENGTH)
+                NextTimePeriod();
         }
     }
+    
+    #endregion
+    
+    #region Public Functions
+    public void SetFlag(Flags flag) { _currentFlags |= flag; }
+    public void RemoveFlag (Flags flag) { _currentFlags &= ~flag; }
+    public void AddEntity (GameObject entity) { _entities.Add(entity); }
+    public void RemoveEntity (GameObject entity) { _entities.Remove(entity); }
+    public void ToggleTimer() { _runTimer = !_runTimer; }
+    #endregion
+
+    #region Private Functions
+
+    private void NextTimePeriod()
+    {
+        //hacer una animacion to guapa para mostrar el paso del tiempo
+        _currentTimePeriod++;
+        UpdateGameState();
+        _timePeriodTimer = 0.0f;
+        /*Debug.Log(_currentTimePeriod);
+        Debug.Log(_currentFlags.ToBinaryString());*/
+    }
+    private void UpdateGameState()
+    {
+        foreach (GameObject entity in _entities)
+        {
+            //Llamar a la cosa que actualice a cada personaje
+            //entity.GetComponent<>().updateState(_currentTimePeriod, _currentFlags);
+        }
+    }
+
+    #endregion
+    
     #endregion
 };
